9d4212323243b964308db35070b1ef708d793585
Aaron <rexforddrive@gmail.com>
2017-10-01 05:11:04 -0700

    Update MySet.js

--- datastructures/MySet.js
+++ datastructures/MySet.js
@@ -35,19 +35,19 @@ class MySet {
     return this.collection.length;
   }
   
-  
+  //filter out values from setB that are not in this and concat them together
   union(setB) {
     return setB.values().filter(x => !this.has(x)).concat(this.values());
   }
-  
+  //return values shared by both sets
   intersection(setB) {
     return this.values().filter(x => setB.has(x));
   }
-  
+  //return values not shared by both sets
   difference(setB) {
     return this.values().filter(x => !setB.has(x));
   }
-  
+  //if setB has one or more values not in this it is not a subset of this
   subset(setB) {
     return setB.difference(this).length === 0;
   }
