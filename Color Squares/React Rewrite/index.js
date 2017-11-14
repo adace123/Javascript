f4e11628da04d3c0657b23c372087fb9d1a9adf3
Aaron <rexforddrive@gmail.com>
2017-11-14 04:46:06 -0800

    Delete index.js

--- Color Squares/React Rewrite/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BoxGrid  from './components/BoxGrid';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<BoxGrid />, document.getElementById('root'));
-registerServiceWorker();
