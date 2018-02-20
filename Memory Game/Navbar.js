dbb18e1e1eff6728042a8df8b0cf927d6ab2662c
Aaron <rexforddrive@gmail.com>
2018-02-20 02:46:12 -0800

    Update Navbar.js

--- Memory Game/Navbar.js
+++ Memory Game/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Stopwatch from './Stopwatch';
 
-export default ({ newGame }) => (
+const Navbar = (props) => {
+  return (
     <nav className="navbar">
-        <h3>Memory Game</h3>
-        <p id="new-game" onClick={newGame}>New Game</p>
+      <h3>Memory Game</h3>
+      <span>Matches: {props.matchCount}</span>
+      <span>Time: <Stopwatch /></span>
+      <p id="new-game" onClick={() => props.startNewGame()}>New Game</p>
     </nav>
-);
+  )
+};
+
+const mapStateToProps = (state) => {
+  return {
+    matchCount: state.matchCount
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    startNewGame: () => dispatch({ type: 'RESET' })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
