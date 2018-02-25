91cd1277323cac1c0b46e989da152af0d49206d2
Aaron <rexforddrive@gmail.com>
2018-02-25 13:26:24 -0800

    Update Navbar.js

--- Memory Game/Navbar.js
+++ Memory Game/Navbar.js
@@ -4,24 +4,33 @@ import Stopwatch from './Stopwatch';
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar">
-      <h3>Memory Game</h3>
-      <span>Matches: {props.matchCount}</span>
-      <span>Time: <Stopwatch /></span>
-      <p id="new-game" onClick={() => props.startNewGame()}>New Game</p>
-    </nav>
+      <nav className="navbar">
+        <h3>Memory Game</h3>
+        <span>Matches: {props.matchCount} / {props.numBoxes / 2}</span>
+        <span>Time: <Stopwatch /></span>
+        <span>Round: {props.rounds}</span>
+        <span id="buttons">
+        <p id="new-game" onClick={() => props.startNewGame()}>New Game</p>
+        <p id="end-game" onClick={() => props.openSaveGameModal()}>End Game</p>
+        <p id="scores" onClick={() => props.openScoreList()}>Scores</p>
+        </span>
+      </nav>
   )
 };
 
 const mapStateToProps = (state) => {
   return {
-    matchCount: state.matchCount
+    matchCount: state.matchCount,
+    rounds: state.rounds,
+    numBoxes: state.numBoxes
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    startNewGame: () => dispatch({ type: 'RESET' })
+    startNewGame: () => dispatch({ type: 'RESET' }),
+    openSaveGameModal: () => dispatch({ type: 'EXIT' }),
+    openScoreList: () => dispatch({ type: 'TOGGLE_SCORELIST', payload: true })
   }
 }
 
