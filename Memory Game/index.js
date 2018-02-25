7de03aa4e7124fee7bc45bf611a7bee26a7793dc
Aaron <rexforddrive@gmail.com>
2018-02-25 13:27:37 -0800

    Update index.js

--- Memory Game/index.js
+++ Memory Game/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
 import MemoryGame from "./MemoryGame";
 import Navbar from "./Navbar";
+import ScoreList from './ScoreList';
+import Alert from './Alert';
+import SaveGame from './SaveGame';
 import store from './store';
-import './style.css';
+import './style.scss';
 
 const App = () =>
   <Provider store={store}>
-    <div className="app">
-      <Navbar />
-      <MemoryGame numBoxes={4} />
-    </div>
+      <div className="app">
+      <Alert />
+        <SaveGame />
+        <ScoreList />
+        <Navbar />
+        <MemoryGame numBoxes={4} />
+      </div>
   </Provider>
 
 render(<App />, document.getElementById('root'));
